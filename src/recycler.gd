dea6fa89f05142263e9dd3705d8df394d68fd712
Rdjrjqouqcu <rdjrjqouqcu@gmail.com>
2026-07-22 22:11:59 -0400

    scenes for robot and recycler, started adding markers for robot pathing

---
+++ src/recycler.gd
@@ -0,0 +1,13 @@
+@icon ("res://addons/at-icons/node2d/factory.svg")
+extends Node2D
+class_name Recycler
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$temp.color = Color(randf_range(0.5, 1.0), randf_range(0.5, 1.0), randf_range(0.5, 1.0))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
---
+++ src/robot.gd
@@ -0,0 +1,10 @@
+extends AnimatableBody2D
+class_name Robot
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(_delta: float) -> void:
+	pass
